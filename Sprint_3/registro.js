30e7eeadf40f216bae89fd1810f29f444a7c6d73
Edwin Martinez M <pixelez@gmail.com>
2021-09-28 20:46:54 +0000

    Sprint 3, función que captura información del formulario

--- Sprint_3/registro.js
+++ Sprint_3/registro.js
@@ -1,1 +1,32 @@
-//Arrays
+// Función para capturar la información del formulario
+function agregarRegistro() {
+    // Constructor
+    function capturas(nombre,apellido,telefono,correo,contrasena) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.contrasena = contrasena;
+    }
+    // Capturar valores
+    var a = document.getElementById("nombre").value;
+    var b = document.getElementById("apellido").value;
+    var c = document.getElementById("telefono").value;
+    var d = document.getElementById("correo").value;
+    var e = document.getElementById("contrasena").value;
+    // Crear el nuevo objeto
+    captura = new capturas(a,b,c,d,e);
+    console.log(captura);
+    contenedor();
+}
+
+// Array con información consignada en formulario
+var arrayCampos = [];
+// Función que contendrá el arreglo
+function contenedor() {
+    arrayCampos.push(captura);
+    console.log(arrayCampos);
+}
+
+// Exportar las funciones
+module.exports = { agregarRegistro, ordenarArreglo, filtrarCorreo }
