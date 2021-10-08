7431cc51930e775bc2623e213c77b28b0c63d382
Edwin Martinez M <pixelez@gmail.com>
2021-10-08 18:35:35 +0000

    Sprint 3, se agrega llamado de las funciones para 10/10

--- Sprint_3/registro.js
+++ Sprint_3/registro.js
@@ -1,50 +1,63 @@
+// Array con información consignada en formulario
 let registros = [];
 
-function agregarRegistro(){
-    function objeto(nombre, apellido, telefono, correo, contrasena){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.telefono = telefono;
-        this.correo = correo;
-        this.contrasena = contrasena;
-    }
-    var objetoNombre = document.getElementById("nombre").value;
-    var objetoApellido = document.getElementById("apellido").value;
-    var objetoTelefono = document.getElementById("telefono").value;
-    var objetoCorreo = document.getElementById("correo").value;
-    var objetoContrasena = document.getElementById("contrasena").value;
-    arreglo = new objeto (objetoNombre, objetoApellido, objetoTelefono, objetoCorreo, objetoContrasena);
-    registros.push(arreglo);
-    console.log(registros);
-
-
-}
-
-function ordenarArreglo(arreglo){
-    registros.sort((a, b) => {
-        const apellidoA = a.apellido.toLowerCase();
-        const apellidoB = b.apellido.toLowerCase();
-        if (apellidoA < apellidoB) {
-            return -1;
-        }
-        if (apellidoA > apellidoB) {
-            return 1;
-        }
+// Función para ordenar el registro por "apellido"
+function ordenarArreglo(arreglo) {
+    arreglo.sort(function(a, b) {
+        let x = a.apellido.toLowerCase();
+        let y = b.apellido.toLowerCase();
+        if (x < y) {return -1;}
+        if (x > y) {return 1;}
         return 0;
     });
-    console.log(registros);
-    return registros;
+    console.log(arreglo);
+    return arreglo;
 }
 
-function filtrarCorreo(arreglo){
-    var correosFiltrados = registros.filter(function(x) {
+// Función para filtrar el correo por "gmail"
+function filtrarCorreo(arreglo) {
+    let filtro = arreglo.filter(function(x) {
         return x.correo.includes("gmail.com")
     });
-    console.log(correosFiltrados);
-    return correosFiltrados;
+    console.log(filtro);
+    return filtro;
 }
 
+// Función para capturar la información del formulario
+function agregarRegistro() {
+    // Constructor
+    class datos {
+        constructor(nombre, apellido, telefono, correo, contrasena) {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.telefono = telefono;
+            this.correo = correo;
+            this.contrasena = contrasena;
+        }
+    }
+    // Capturar valores
+    var a = document.getElementById("nombre").value;
+    var b = document.getElementById("apellido").value;
+    var c = document.getElementById("telefono").value;
+    var d = document.getElementById("correo").value;
+    var e = document.getElementById("contrasena").value;
+    // Crear el nuevo objeto
+    arreglo = new datos(a,b,c,d,e);
+    contenedor();
+}
+// Función que contendrá el arreglo
+function contenedor() {
+    registros.push(arreglo);
+    // ordenarArreglo(registros);
+    console.log(registros);
+}
+
+// LLamada de las funciones
+ordenarArreglo(registros);
+filtrarCorreo(registros);
+
+// Exportar las funciones
 module.exports.registros = registros;
-module.exports.agregarRegistro = agregarRegistro;
-module.exports.ordenarArreglo = ordenarArreglo;
 module.exports.filtrarCorreo = filtrarCorreo;
+module.exports.ordenarArreglo = ordenarArreglo;
+module.exports.agregarRegistro = agregarRegistro;
